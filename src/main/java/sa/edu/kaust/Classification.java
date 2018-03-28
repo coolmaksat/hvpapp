cc17c5f5e448aab6ec3534623130b00dc1c41bbc
imene88 <imene88@users.noreply.github.com>
2018-03-28 15:13:30 +0300

    Update Classification.java

--- src/main/java/sa/edu/kaust/Classification.java
+++ src/main/java/sa/edu/kaust/Classification.java
@@ -14,6 +14,10 @@ public class Classification {
 
     public String modelFile = "";
     public String[] topLevelPhenotypes;
+	public Map<String, List<String> > actions;
+	List<String> genes;
+	public Map<String, List<String> > geneVars;
+
 
     public Classification(Properties props) throws Exception {
         // Loading the saved classifier
@@ -23,7 +27,6 @@ public class Classification {
 
 
     public void toolClassify(String fileName) throws Exception {
-        //FileReader fr = new FileReader(fileName + ".csv");
 		long fileCount = Files.lines(Paths.get(fileName + ".csv")).count() - 1;
 		Double[] results = new Double[(int)fileCount];
         Classification that = this;
@@ -110,7 +113,7 @@ public class Classification {
         out.close();
 }
 
-	public void toolDigenic(String fileName, Map<String, List<String> > interactions, int param) throws Exception {
+	public void toolDigenic(String fileName, Map<String, List<String> > interactions, int param, int topCount) throws Exception {
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".digenic"), 1073741824));
 		for (int i = 1; i<=param; i++) {
 		out.print("Chr"+i+"\tStart"+i+"\tRef"+i+"\tAlt"+i+"\tGT"+i+"\tGene"+i+"\tCADD\tSim_Score\tPrediction_Score"+i+"\t");
@@ -125,7 +128,6 @@ public class Classification {
 			br.close();
 		//process combinations
 		List<DataResult> data = new ArrayList<DataResult>();
-		//int index = 0;
 		for(int i=0; i < vars.size(); i++){
 			double combined_score = 0.0;
 			String[] set1 = vars.get(i).split("\t", -1);
@@ -150,7 +152,7 @@ public class Classification {
 				String second_gene = vars.get(j).split("\t", -1)[5];
 				double second_pred = Double.parseDouble(vars.get(j).split("\t", -1)[10]);
 				//check if both genes interact
-				if ((interactions.get(first_gene).contains(second_gene)) || first_gene.equals(second_gene)){
+				if ((interactions.get(first_gene).contains(second_gene))){
 					combined_score = first_pred + second_pred;
 					s = first_var + "\t" + second_var;
 					DataResult mydata = new DataResult(s, combined_score);
@@ -159,16 +161,23 @@ public class Classification {
 			}
 		}
 		Collections.sort(data, Collections.reverseOrder());
-		for (int i = 0; i < data.size(); i++) {
-            out.println(data.get(i).s + "\t" + data.get(i).r);
-        }
+		if (topCount < data.size()) {
+			for (int i = 0; i < topCount; i++) {
+				out.println(data.get(i).s + "\t" + data.get(i).r);
+			}
+		}
+		else {
+			for (int i = 0; i < data.size(); i++) {
+				out.println(data.get(i).s + "\t" + data.get(i).r);
+			}
+		}
         out.flush();
         out.close();
 		}
     }
 
-	public void toolTrigenic(String fileName, Map<String, List<String> > interactions, int param) throws Exception {
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".trigenic"), 1073741824));
+	public void toolTrigenic(String fileName, Map<String, List<String> > interactions, int param, int topCount) throws Exception {
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".trigenic"), 1073741824));
 		for (int i = 1; i<=param; i++) {
 		out.print("Chr"+i+"\tStart"+i+"\tRef"+i+"\tAlt"+i+"\tGT"+i+"\tGene"+i+"\tCADD\tSim_Score\tPrediction_Score"+i+"\t");
 		}
@@ -182,8 +191,9 @@ public class Classification {
 			br.close();
 		//process combinations
 		List<DataResult> data = new ArrayList<DataResult>();
-		//int index = 0;
+		List<Double> dataScores = new ArrayList<Double>();
 		for(int i=0; i < vars.size(); i++){
+			System.out.println(i);
 			double combined_score = 0.0;
 			String[] set1 = vars.get(i).split("\t", -1);
 			String first_var = "";
@@ -193,8 +203,8 @@ public class Classification {
 			}
 			first_var += set1[9] + "\t";
 			first_var += set1[10];
-			String first_gene = vars.get(i).split("\t", -1)[5];
-			double first_pred = Double.parseDouble(vars.get(i).split("\t", -1)[10]);
+			String first_gene = set1[5];
+			double first_pred = Double.parseDouble(set1[10]);
 			//second loop
 			for (int j=i+1; j < vars.size(); j++) {
 				String[] set2 = vars.get(j).split("\t", -1);
@@ -204,8 +214,9 @@ public class Classification {
 				}
 				second_var += set2[9] + "\t";
 				second_var += set2[10];
-				String second_gene = vars.get(j).split("\t", -1)[5];
-				double second_pred = Double.parseDouble(vars.get(j).split("\t", -1)[10]);
+				String second_gene = set2[5];
+				double second_pred = Double.parseDouble(set2[10]);
+				//third loop
 				for (int k=j+1; k < vars.size(); k++) {
 					String[] set3 = vars.get(k).split("\t", -1);
 					String third_var = "";
@@ -214,47 +225,258 @@ public class Classification {
 						}
 					third_var += set3[9] + "\t";
 					third_var += set3[10];
-					String third_gene = vars.get(k).split("\t", -1)[5];
-					double third_pred = Double.parseDouble(vars.get(k).split("\t", -1)[10]);
-					//check if both genes interact
+					String third_gene = set3[5];
+					double third_pred = Double.parseDouble(set3[10]);
 					//if genes are identical
 					if ((first_gene.equals(second_gene)) && (second_gene.equals(third_gene))) {
 						combined_score = first_pred + second_pred + third_pred;
 						s = first_var + "\t" + second_var + "\t" + third_var;
-						DataResult mydata = new DataResult(s, combined_score);
-						data.add(mydata);
+						if (data.size() < topCount) { 
+							DataResult mydata = new DataResult(s, combined_score);
+							data.add(mydata);
+							dataScores.add(combined_score);
+						}
+						else { 
+							double minVal = Collections.min(dataScores);
+							if ( minVal < combined_score ) {
+								int minIndex = dataScores.indexOf(minVal);
+								DataResult mydata = new DataResult(s, combined_score);
+								data.set(minIndex, mydata);
+								dataScores.set(minIndex,combined_score); 
+							}
+						}
 					}
 					else {
 						if ((interactions.get(first_gene).contains(second_gene)) && (interactions.get(first_gene).contains(third_gene))){
 							combined_score = first_pred + second_pred + third_pred;
 							s = first_var + "\t" + second_var + "\t" + third_var;
-							DataResult mydata = new DataResult(s, combined_score);
-							data.add(mydata);
+							if (data.size() < topCount) { 
+								DataResult mydata = new DataResult(s, combined_score);
+								data.add(mydata);
+								dataScores.add(combined_score);
+							}
+							else { 
+								double minVal = Collections.min(dataScores);
+								if ( minVal < combined_score ) {
+									int minIndex = dataScores.indexOf(minVal);
+									DataResult mydata = new DataResult(s, combined_score);
+									data.set(minIndex, mydata);
+									dataScores.set(minIndex,combined_score); 
+								}
+							}
 						}
 						else if ((interactions.get(first_gene).contains(second_gene)) && (interactions.get(second_gene).contains(third_gene))){
 							combined_score = first_pred + second_pred + third_pred;
 							s = first_var + "\t" + second_var + "\t"+ third_var;
-							DataResult mydata = new DataResult(s, combined_score);
-							data.add(mydata);
+							if (data.size() < topCount) { 
+								DataResult mydata = new DataResult(s, combined_score);
+								data.add(mydata);
+								dataScores.add(combined_score);
+							}
+							else { 
+								double minVal = Collections.min(dataScores);
+								if ( minVal < combined_score ) {
+									int minIndex = dataScores.indexOf(minVal);
+									DataResult mydata = new DataResult(s, combined_score);
+									data.set(minIndex, mydata);
+									dataScores.set(minIndex,combined_score); 
+								}
+							}
 						}
 						else if ((interactions.get(first_gene).contains(third_gene)) && (interactions.get(third_gene).contains(second_gene))){
 							combined_score = first_pred + second_pred + third_pred;
 							s = first_var + "\t" + second_var + "\t"+ third_var;
-							DataResult mydata = new DataResult(s, combined_score);
-							data.add(mydata);
+							if (data.size() < topCount) { 
+								DataResult mydata = new DataResult(s, combined_score);
+								data.add(mydata);
+								dataScores.add(combined_score);
+							}
+							else { 
+								double minVal = Collections.min(dataScores);
+								if ( minVal < combined_score ) {
+									int minIndex = dataScores.indexOf(minVal);
+									DataResult mydata = new DataResult(s, combined_score);
+									data.set(minIndex, mydata);
+									dataScores.set(minIndex,combined_score); 
+								}
+							}
 						}
 					}
 				}
 			}
 		}
 		Collections.sort(data, Collections.reverseOrder());
-		for (int i = 0; i < data.size(); i++) {
-            out.println(data.get(i).s + "\t" + data.get(i).r);
-        }
+		if (topCount < data.size()) {
+			for (int i = 0; i < topCount; i++) {
+				out.println(data.get(i).s + "\t" + data.get(i).r);
+			}
+		}
+		else {
+			for (int i = 0; i < data.size(); i++) {
+				out.println(data.get(i).s + "\t" + data.get(i).r);
+			}
+		}
         out.flush();
         out.close();
 		}
     }
+	public void toolTrigenicOpt(String fileName, Map<String, List<String> > interactions, int param, int topCount) throws Exception {
+		this.actions = new HashMap<String, List<String> >();
+		this.genes = new ArrayList<String>();
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".trigenic"), 1073741824));
+		for (int i = 1; i<=param; i++) {
+		out.print("Chr"+i+"\tStart"+i+"\tRef"+i+"\tAlt"+i+"\tGT"+i+"\tGene"+i+"\tCADD\tSim_Score\tPrediction_Score"+i+"\t");
+		}
+		out.println("Combined_Score");
+		this.geneVars = new HashMap<String, List<String> >();
+		try(BufferedReader br = Files.newBufferedReader(Paths.get(fileName + ".top"))) {
+            String line;
+			List<String> vars = new ArrayList<String>();
+            while((line = br.readLine()) != null) {
+                vars.add(line);
+				String[] items = line.split("\t", -1);
+				String gene = items[5];
+				if (!this.geneVars.containsKey(gene)) {
+					this.geneVars.put(gene, new ArrayList<String>());
+				}
+				List<String> varSet = this.geneVars.get(gene);
+				varSet.add(line);
+			}
+			br.close();
+		//process combinations
+		List<DataResult> data = new ArrayList<DataResult>();
+		List<Double> dataScores = new ArrayList<Double>();
+		List<String> combo = new ArrayList<String>();
+		this.genes.addAll(geneVars.keySet());
+		for (String g: this.genes) {
+			if (!this.actions.containsKey(g)) {
+				this.actions.put(g, new ArrayList<String>());
+			}
+			List<String> actionSet = this.actions.get(g);
+			for (String intG : interactions.get(g)) {
+				if (genes.contains(intG))
+					actionSet.add(intG);
+			}
+		}
+		for (String g: this.genes) {
+			if (this.geneVars.get(g).size() == 3) {
+				double combined_score = 0.0;
+				String[] set1 = this.geneVars.get(g).get(0).split("\t", -1);
+				String[] set2 = this.geneVars.get(g).get(1).split("\t", -1);
+				String[] set3 = this.geneVars.get(g).get(2).split("\t", -1);
+				String first_var = "";
+				String second_var = "";
+				String third_var = "";
+				String s = "";
+				for (int x=0; x < 7; x++){
+					first_var += set1[x] + "\t";
+					second_var += set2[x] + "\t";
+					third_var += set3[x] + "\t";
+				}
+				first_var += set1[9] + "\t";
+				second_var += set2[9] + "\t";
+				third_var += set3[9] + "\t";
+				first_var += set1[10];
+				second_var += set2[10];
+				third_var += set3[10];
+				double first_pred = Double.parseDouble(set1[10]);
+				double second_pred = Double.parseDouble(set2[10]);
+				double third_pred = Double.parseDouble(set3[10]);
+				combined_score = first_pred + second_pred + third_pred;
+				s = first_var + "\t" + second_var + "\t" + third_var;
+				if (data.size() < topCount) { //add to list directly
+					DataResult mydata = new DataResult(s, combined_score);
+					data.add(mydata);
+					dataScores.add(combined_score);
+					combo.add(s);
+				}
+				else { 
+					double minVal = Collections.min(dataScores);
+					if ( minVal < combined_score ) {
+						int minIndex = dataScores.indexOf(minVal);
+						DataResult mydata = new DataResult(s, combined_score);
+						data.set(minIndex, mydata);
+						dataScores.set(minIndex,combined_score); 
+						combo.set(minIndex, s);
+					}
+				}
+			}
+			//process all combinations of genes interacting!
+			int count = this.actions.get(g).size();
+			String g1 = g;
+			for (int i = 0; i < count; i++) {
+				String g2 = this.actions.get(g).get(i);
+				for (int j = i+1; j < count; j++) {
+					String g3 = this.actions.get(g).get(j);
+					for (String var1: this.geneVars.get(g1)) {
+						for (String var2: this.geneVars.get(g2)) {
+							for (String var3: this.geneVars.get(g3)) {
+								if ((!var1.equals(var2)) && (!var2.equals(var3)) && (!var1.equals(var3))) {
+									double combined_score = 0.0;
+									String[] set1 = var1.split("\t", -1);
+									String[] set2 = var2.split("\t", -1);
+									String[] set3 = var3.split("\t", -1);
+									String first_var = "";
+									String second_var = "";
+									String third_var = "";
+									String s = "";
+									for (int x=0; x < 7; x++){
+										first_var += set1[x] + "\t";
+										second_var += set2[x] + "\t";
+										third_var += set3[x] + "\t";
+									}
+									first_var += set1[9] + "\t";
+									second_var += set2[9] + "\t";
+									third_var += set3[9] + "\t";
+									first_var += set1[10];
+									second_var += set2[10];
+									third_var += set3[10];
+									double first_pred = Double.parseDouble(set1[10]);
+									double second_pred = Double.parseDouble(set2[10]);
+									double third_pred = Double.parseDouble(set3[10]);
+									combined_score = first_pred + second_pred + third_pred;
+									s = first_var + "\t" + second_var + "\t" + third_var;
+									boolean found = checkPermutation(first_var, second_var, third_var, combo);
+									if (!found) {
+										if (data.size() < topCount) { 
+											DataResult mydata = new DataResult(s, combined_score);
+											data.add(mydata);
+											dataScores.add(combined_score);
+											combo.add(s);
+										}
+										else { 
+											double minVal = Collections.min(dataScores);
+											if ( minVal < combined_score ) {
+												int minIndex = dataScores.indexOf(minVal);
+												DataResult mydata = new DataResult(s, combined_score);
+												data.set(minIndex, mydata);
+												dataScores.set(minIndex,combined_score); 
+												combo.set(minIndex, s);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		Collections.sort(data, Collections.reverseOrder());
+		if (topCount < data.size()) {
+			for (int i = 0; i < topCount; i++) {
+				out.println(data.get(i).s + "\t" + data.get(i).r);
+			}
+		}
+		else {
+			for (int i = 0; i < data.size(); i++) {
+				out.println(data.get(i).s + "\t" + data.get(i).r);
+			}
+		}
+        out.flush();
+        out.close();
+		}
+}
 	public void toolCombine(String fileName, UndirectedGraph<String, DefaultEdge> actions, int param) throws Exception {
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".digenic"), 1073741824));
 		for (int i = 1; i<=param; i++) {
@@ -437,6 +659,33 @@ public class Classification {
     }
     return combinations;
 }
+
+	public static boolean checkPermutation(String first_var, String second_var, String third_var, List<String> combo) {
+		String s1,s2,s3,s4,s5,s6;
+		s1 = first_var + "\t" + second_var + "\t" + third_var;
+		s2 = first_var + "\t" + third_var + "\t" + second_var;
+		s3 = second_var + "\t" + third_var + "\t" + first_var;
+		s4 = second_var + "\t" + first_var + "\t" + third_var;
+		s5 = third_var + "\t" + first_var + "\t" + second_var;
+		s6 = third_var + "\t" + second_var + "\t" + first_var;
+
+		if (combo.contains(s1))
+			return true;
+		else if (combo.contains(s2))
+			return true;
+		else if (combo.contains(s3))
+			return true;
+		else if (combo.contains(s4))
+			return true;
+		else if (combo.contains(s5))
+			return true;
+		else if (combo.contains(s6))
+			return true;
+		else
+			return false;
+
+}
+
 	public static <String> List<String> getSublist(List<String> list, int i) {
 		List<String> sublist = new ArrayList<String>();
 		for (int j = i; j < list.size(); j++) {
