49879422e14d26b584eceb2345174fa0266173e9
imene88 <imene88@users.noreply.github.com>
2018-12-08 00:49:48 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import keras
+from keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import pandas as pd
+import os
+import sys
+import h5py
+import tensorflow as tf
+
+print("Hello World!")
