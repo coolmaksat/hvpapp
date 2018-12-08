8081beb78024d8eee4463cd329d7a1735ca0b9bc
imene88 <imene88@users.noreply.github.com>
2018-12-08 21:10:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,5 +8,10 @@ import os
 import sys
 import h5py
 import tensorflow as tf
+import platform
 
-print("Hello World!")
+s=platform.python_version()
+if "2.7" not in s:
+	print("Please install Python version 2.7 and symlink the command [python] to Python 2.7 excutable.")
+else:
+	print("Hello World!")
